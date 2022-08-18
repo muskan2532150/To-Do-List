2d47cab7f83616d15891b4c2ccc3d91cd4f6db25
muskan2532150 <rg2532150@gmail.com>
2022-08-18 18:29:40 +0530

    fix linter error

--- add.test.js
+++ add.test.js
@@ -1,8 +1,8 @@
 /* eslint-disable no-unused-vars */
-import Display from './module/Display';
+import Display from './module/Display.js';
 
 const { default: JSDOMEnvironment } = require('jest-environment-jsdom');
-const LocalStorage = require('./module/LocalStorage');
+const LocalStorage = require('./module/LocalStorage.js');
 /**
  * @jest-environment jsdom
  */
